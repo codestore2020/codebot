aa9b0548f5c9b2e0549366a72a8f8b5f7aa26d30
codestore2020 <65408340+codestore2020@users.noreply.github.com>
2020-06-14 17:54:14 +0200

    Revert "bot"
    This reverts commit 51a3cee853a1b33050858a7a7f6993f341a241d8.

--- commands/close.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message) => {
-    
-    if (!(message.channel.name.startsWith("🎫"))) return message.channel.send("Dit commando werkt alleen in ticket channels");
-
-    message.channel.delete();
- 
-}
